f0347727489c5af5444a72d297b72149a33229ae
anishsaini <anishoficial@gmail.com>
2024-09-22 09:35:38 +0530

    Its just learning App

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function home() {
+  return (
+    <View>
+      <Text style={{ color: "black", fontSize: 40 }}>home</Text>
+    </View>
+  );
+}
