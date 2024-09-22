6ccbaab8b71947edbe7ca16eaa63d746488b5616
anishsaini <anishoficial@gmail.com>
2024-09-22 09:34:57 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,17 +15,6 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
